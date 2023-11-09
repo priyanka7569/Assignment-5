cdc0636bf12fd915504f811a356da4d8caefc904
priyanka7569 <kota.priyanka@mphasis.com>
2023-11-09 15:36:31 +0530

    this is my fifth program

---
+++ Assignment 5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 1: Declare and initialize variables
+            string name = "priyanka";
+            int age = 23;  // Replace 25 with your actual age
+            double height = 5.25;  // Replace 1.75 with your actual height in meters
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age} years");
+            Console.WriteLine($"Height: {height} meters");
+
+            Console.ReadKey();
+
+        }
+    }
+}
